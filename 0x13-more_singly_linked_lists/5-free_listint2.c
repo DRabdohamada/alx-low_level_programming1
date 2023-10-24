f3c8029732d9d483f2b3755a072a30edf2443cc7
DRabdohamada <drabdelrahmanhamada29@gmail.com>
2023-10-24 19:37:56 +0000

    m

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint2 - free a node and begin drom the head
+ *
+ * @head: the begin
+ *
+ * Return: if error
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+
+		return (0);
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+
+		free(*head);
+
+		*head = temp;
+	}
+}
