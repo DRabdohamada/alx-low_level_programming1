535a5f735eecc8034391081489b0b8064f28f008
DRabdohamada <drabdelrahmanhamada29@gmail.com>
2023-10-24 15:03:19 +0000

    c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - is a function to print the counter
+ *
+ * @h: is the node to print
+ *
+ * Return: the count
+ */
+
+size_t listint_len(const listint_t *h);
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+
+		h = h->next;
+
+		counter++;
+
+	}
+
+	return (counter);
+
+
+
+
+}
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,5 +20,6 @@ struct list *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
